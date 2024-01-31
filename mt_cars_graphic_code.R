1e92aadcc80a20be5c8649355270281ef4fd078b
IzzyBedic <izzy@bedichek.org>
2024-01-31 09:10:52 -0500

    used stringr to create a variable containing only mercedes (needed a tibble to make it more intuitive to view)

--- mt_cars_graphic_code.R
+++ mt_cars_graphic_code.R
@@ -1,7 +1,15 @@
 # Libraries
 library(dplyr)
+library(stringr)
 
 # Making sure all data is in a column
 mt_cars_all_columns <- mtcars %>% 
-  tibble::rownames_to_column("model")
+  tibble::rownames_to_column("model") %>% 
+  tibble()
+
   
+# Creating a variable that contains only the Mercedes cars
+mt_car_mercedes <- mt_cars_all_columns %>% 
+  mutate(model = str_to_lower(model, locale = "en")) %>% 
+  filter(str_detect(model, as.character("merc(.*)")))
+                  
